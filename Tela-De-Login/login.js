2d484990ebe83c8a8e32643d4e4e6bc86a62b2ff
MarianeCaldeira <87836763+MarianeCaldeira@users.noreply.github.com>
2021-10-11 17:53:31 -0300

    Add files via upload

---
+++ Tela-De-Login/login.js
@@ -0,0 +1,42 @@
+var nome = "";
+var senha = "";
+
+
+function validar() {
+
+nome = document.getElementById("login").value;
+senha = document.getElementById("senha").value;
+
+ if (nome === "" || senha === "") {
+    alert("Por favor verifique se todos os dados foram preenchidos.");
+
+    return ;
+    
+  }
+
+  else {
+      alert("Seja bem vinde ao Restaurante Delícias Brasileiras!")
+  }
+
+}
+
+function esqueceusenha() {
+
+    alert("Para recuperar a senha, entre em contato com o restaurante.");
+}
+
+
+function cadastreum() {
+
+  window.open("https://www.facebook.com/", "_blank")
+}
+
+function cadastredois() {
+
+  window.open("https://www.gmail.com", "_blank")
+}
+
+function cadastre3() {
+
+  alert("Estamos em manutenção.");
+}
