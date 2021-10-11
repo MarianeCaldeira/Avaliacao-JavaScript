2f4e83970da73f3bc3248b38d65cbdcce6488a9b
MarianeCaldeira <87836763+MarianeCaldeira@users.noreply.github.com>
2021-10-10 23:11:23 -0300

    Add files via upload

---
+++ Tela-De-Home/home.js
@@ -0,0 +1,40 @@
+/*Se algum link nao foi criado, deverá colocar a posição dentro do array*/
+var pagina = document.getElementsByTagName("a")[0];
+pagina.addEventListener("click", () => {
+    pagina = pagina.getAttribute("href");
+    if (pagina === "#"){
+        alert("A tela não foi encontrada. Entre em contato com o restaurante.");
+    }
+})
+
+var pagina = document.getElementsByTagName("a")[1];
+pagina.addEventListener("click", () => {
+    pagina = pagina.getAttribute("href");
+    if (pagina === "#"){
+        alert("A tela não foi encontrada. Entre em contato com o restaurante.");
+    }
+})
+
+var pagina = document.getElementsByTagName("a")[2];
+pagina.addEventListener("click", () => {
+    pagina = pagina.getAttribute("href");
+    if (pagina === "#"){
+        alert("A tela não foi encontrada. Entre em contato com o restaurante.");
+    }
+})
+
+var pagina = document.getElementsByTagName("a")[3];
+pagina.addEventListener("click", () => {
+    pagina = pagina.getAttribute("href");
+    if (pagina === "#"){
+        alert("A tela não foi encontrada. Entre em contato com o restaurante.");
+    }
+})
+
+var pagina = document.getElementsByTagName("a")[4];
+pagina.addEventListener("click", () => {
+    pagina = pagina.getAttribute("href");
+    if (pagina === "#"){
+        alert("A tela não foi encontrada. Entre em contato com o restaurante.");
+    }
+})
